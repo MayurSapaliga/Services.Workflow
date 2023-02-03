e85d5e9bca797d7d94f54586d644ac27c8aa4663
Mayur Sapaliga <msapaliga@collabware.com>
2023-02-03 11:58:58 -0800

    Add project files.

---
+++ Services.Workflow/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Services.Workflow
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string PrintWorkflow()
+        {
+            string message = "Hello from workflow service";
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
